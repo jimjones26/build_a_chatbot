8fb914f44340d768cf6dde4c6720ad85a886857d
jjones <jimmy.jones@tutanota.com>
2024-09-29 15:37:15 -0600

    Add message persistence and state graph integration to chatbot
    - Integrated `MemorySaver` for message persistence.
    - Added `StateGraph` to manage message states.
    - Implemented asynchronous function to call the model with state messages.
    - Configured workflow with memory and custom configuration.
    - Enhanced chatbot interaction with persistent memory and state management.

--- src/build_a_chatbot.py
+++ src/build_a_chatbot.py
@@ -3,6 +3,8 @@
 # ------------------------------------------------------------------
 from langchain_ollama import ChatOllama
 from langchain_core.messages import HumanMessage, AIMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
 
 # ------------------------------------------------------------------
 # Create model
@@ -14,6 +16,9 @@ model = ChatOllama(
     num_ctx=16384,
 )
 
+# ------------------------------------------------------------------
+# Interact with model without memory
+# ------------------------------------------------------------------
 model.invoke([HumanMessage(content="Hi! I'm Jimmy")])
 
 model.invoke([HumanMessage(content="What's my name?")])
@@ -27,3 +32,33 @@ model.invoke(
         HumanMessage(content="What is the Etymology of my name?"),
     ]
 )
+
+# ------------------------------------------------------------------
+# Add message persistence
+# ------------------------------------------------------------------
+# Define a new graph
+workflow = StateGraph(state_schema=MessagesState)
+
+
+# Define the function that calls the model
+async def call_model(state: MessagesState):
+    response = model.invoke(state["messages"])
+    return {"messages": response}
+
+
+# Define the (singal) node in the graph
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+# Add memory
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+
+# Add config
+config = {"configurable": {"thread_id": "abc123"}}
+
+query = "Hello, my name is liberaci."
+
+input_messages = [HumanMessage(query)]
+output = await app.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print()
